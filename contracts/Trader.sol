443ed12490ebd4e914f68ce319a69bbe91aba2a2
John <jastek@SEBEK.localdomain>
2023-02-28 04:19:08 -0800

    WIP: update Trader.sol

--- contracts/Trader.sol
+++ contracts/Trader.sol
@@ -10,22 +10,21 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 
 contract Trader {
-    // pass in FLP contract address save to State var - NOTE: must deploy FLP first
-    // NOTE: be sure to pass tokens in test
-    address constant flashLoanPool = 0x0; // NEED FlashloanPool address
-    uint256 borrowAmount;
-    
-
+    // pass in FLP contract address save to State var - NOTE: must deploy FLP first. Be sure to pass tokens in test
+   
+   // address constant flashLoanPool = 0x0; // NEED FlashloanPool address will not compile
+    uint256 borrowAmount;    
     address public owner;
     
-    constructor(address token1, address _flashLoanPool) payable {
-        owner = msg.sender;
+    constructor(address _token1, address _flashLoanPoolAddress) payable {
+        owner = msg.sender;        
+        flashLoanPoolAddress = _flashLoanPoolAddress;
     }     
     
     // call FLP similar to IReceiver(msg.sender).receiveTokens(address(token), borrowAmount);
     // will be Public
     function flashloan(address _token1, uint256 _borrowAmount) public {       
-        flashLoanPool(msg.sender).receiveTokens(address(_token1), _borrowAmount);    
+        FlashLoanPool(msg.sender).receiveTokens(address(_token1), _borrowAmount);    
     }
 
     
@@ -43,14 +42,12 @@ contract Trader {
     
 
 
-
     // same as FLP receive function - see above
     // will be Public
     function depositLoan(address token1, uint256 _borrowAmount) public payable {
-        flashLoanPool.receiveTokens(address(token1), _borrowAmount);   
+        FlashLoanPool(flashLoanPoolAddress).receiveTokens(address(token1), _borrowAmount);   
     
     // do something with the money
 
     }
 }
-
