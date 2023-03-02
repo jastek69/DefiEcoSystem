ccacbb076b26267514b34eb11e56c4c68ad775d8
John <jastek@SEBEK.localdomain>
2023-03-01 18:01:56 -0800

    update FlashLoan.js testing

--- test/FlashLoan.js
+++ test/FlashLoan.js
@@ -1,12 +1,5 @@
-// -- IMPORT PACKAGES -- //
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { Contract } from "hardhat/internal/hardhat-network/stack-traces/model";
-
-
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
-
-// require("dotenv").config();
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
@@ -15,39 +8,47 @@ const tokens = (n) => {
 const ether = tokens
 const shares = ether
 
-// const FlashLoan = artifacts.require('./FlashLoan')
+describe('FlashLoan', () => {  
+ 
+  it('Borrowing 1M SOB and throws revert info msg.', async () => {
 
-describe('FlashLoan', () => {
-  // Accounts
-  let deployer,
-      trade
+    // [X] Deploy token
+    // [X] Deploy FLP
+    // [] transfer tokens to FLP
+    // [] Deploy trader contract
+    // [] Fetch accounts from ethers js library ()getSigners
+    // [] Call Flashloan function on the Trader contract and make sure it works
 
-  // Contracts    
-  let token1,
-      Trader
-
-
-beforeEach(async () => {
-  trade = await ethers.getSigners()
-  deployer = trade[1]
-})
-
-  it('Borrowing 1M USD and throws revert info msg.', async () => {
+    // Deploy Token
     const Token = await ethers.getContractFactory('Token')
-    let token1 = await Token.deploy('USD Token', 'USD', tokens('10000000')) // Deploy 1 Billion tokens
+    let token = await Token.deploy('SOB Token', 'SOB', tokens('1000000000')) // Deploy 1 Billion tokens
 
-    const Trader = await ethers.getContractFactory('Trader')
-    let trader = await Trader.deploy()
     
-    trade = await ethers.getSigners()
-    deployer = trade[0]
+    // Deploy Flash Loan Pool contract
+    console.log(`Deploying FlashLoanPool contract...\n`)
+    const FlashLoanPool = await ethers.getContractFactory("FlashLoanPool")
+    const flashLoanPool = await FlashLoanPool.deploy(token.address)
+    await flashLoanPool.deployed()
+    console.log(`FlashLoanPool deployed to: ${flashLoanPool.address}\n`);
 
-    await token1.approve(trader.address, tokens(1000000))
 
-    //Deposit
-    await trader.deposit(token1.address, tokens(1000000))
+    // Transfer tokens to FlashLoanPool
+    // call depositTokens function from FLP and place 1000000 
 
-    expect(await token1.balanceOf(trader.address)).to.equal(tokens(1000000))
-    })        
+    // Deploy Trader contract
+    // const Trader = await ethers.getContractFactory('Trader')
+    // let trader = await Trader.deploy()
+    
+    // trade = await ethers.getSigners()
+    // deployer = trade[0]
+
+    // await token1.approve(trader.address, tokens(1000000))
+
+    // //Deposit
+    // await trader.deposit(token1.address, tokens(1000000))
+
+    // expect(await token1.balanceOf(trader.address)).to.equal(tokens(1000000))
+            
   })          
 
+})
---
+++ test/WIPexecute-flashloan.js
@@ -0,0 +1,74 @@
+const hre = require("hardhat")
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const tokens = (n) => {
+    return ethers.utils.parseUnits(n.toString(), 'ether')
+  }
+  
+const ether = tokens
+const shares = ether
+
+// describe('FlashLoan', () => {
+//     // Accounts
+//     let account,
+//         flashLoanPool
+
+//     // Contracts
+//     let token1,
+//         trader,
+//         amm1,
+//         amm2
+
+
+    
+    
+    
+// QUESTIONS:
+//Should I place all tests here or create another DES test file
+// explain describe
+// When to use beforeEach(async () => {  })
+// 
+// Where to view transcation
+//console.log(`-- View Transaction --`)
+// console.log(`https://polygonscan.com/tx/${result.transactionHash}`)
+
+
+
+
+
+    async function main() {
+        console.log(`Preparing Transaction...\n`)
+
+        // Fetch the deployed contract
+        const address = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+        const FlashLoanPool = await hre.ethers.getContractAt("FlashLoanPool", address)
+        console.log(`Contract fetched: ${FlashLoanPool.address}`)
+
+        // Fetch account
+        const [account] = await hre.ethers.getSigners()
+        console.log(`Account fetched: ${account.address}\n`)
+
+        // Perform flashloan
+        const pool = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
+        const SOB = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+        const amount = hre.ethers.utils.parseUnits('100000', "ether")
+
+        console.log(`Executing Flashloan...\n`)
+
+        let transaction = await FlashLoanPool.connect(account).flashLoan(amount)
+        const result = await transaction.wait()
+
+        console.log(`Transaction Successful!\n`)
+        // console.log(`-- View Transaction --`)
+        // console.log(`https://polygonscan.com/tx/${result.transactionHash}`)
+    }
+
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+ // })
