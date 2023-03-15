26fb663c2ee2522cce22337248105a8f24cd4cf5
John <jastek@SEBEK.localdomain>
2023-03-15 05:42:48 -0700

    WIP:changed arbitrage function

--- contracts/Trader.sol
+++ contracts/Trader.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "./Token.sol";
 import "./FlashLoanPool.sol";
+import "./AMM1.sol";
+import "./AMM2.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -28,6 +30,18 @@ contract Trader {
         uint256 tokenGiveAmount      
      );
     
+    // Events
+        // emit Buy(
+        //     address(owner),
+        //     _numberOfTokens
+        // );
+
+        // emit Sell(
+        //     address(owner),
+        //     tokensSold
+        // );
+
+
     constructor(address _token1, address _flashLoanPoolAddress) payable {
         owner = msg.sender;
         token1 = _token1;   // USD token in Flash pool
@@ -46,47 +60,88 @@ contract Trader {
         address(token1),    // tokenGive,
         _borrowAmount      // tokenGiveAmount       
         );
+        
+        
     
-
-    // Do something with the money here
+    // Do something with the money here - call arbitrage function    
     // Arbitrage here buy on AMM1 and sell on AMM2
-    // use script to get prices to change which will call trader contract to buy and sell
+    // TODO: advanced - use script to get prices to change which will call trader contract to buy and sell     
+
+    arbitrage(AMM1, AMM2, token1, _borrowAmount );
 
     
-    function buyTokens(uint256 _numberOfTokens) public payable {
-        require(msg.value == multiply(_numberOfTokens, tokenPrice));
-        require(amm2Token2.balanceOf(this) >= _numberOfTokens);
-        require(amm2Token2.transfer(msg.sender, _numberOfTokens));
-
-        tokensSold += _numberOfTokens; 
-
-        Sell(msg.sender, _numberOfTokens);
-    }
-
-
-    function arbitrage(AMM1, AMM2, uint256 buyAmount,uint256 sellAmount) external nonReentrant {
-        require(buyAmount > 0, "Must buy at least one token");
-        require(sellAmount > 0, "Must sell at least one token");
-
-        uint256 balanceBefore = token.balanceOf(address(this));
-        require(balanceBefore >= buyAmount, "Not enough tokens in pool");
-
-
-        // Buy tokens on AMM1
-        token.approve(address(AMM1), buyAmount);
-        AMM1.buyTokens(buyAmount);
-
-        // Sell tokens on AMM2
-        token.approve(address(AMM2), sellAmount);
-        AMM2.sellTokens(sellAmount);
-
-        uint256 balanceAfter = token.balanceOf(address(this));
-        require(balanceAfter >= balanceBefore, "Arbitrage was not successful");
-    }        
-
     // Return all tokens to the Pool - 
     require(Token(token1).transfer(msg.sender, _borrowAmount), "Transfer of tokens failed");
 
-    } 
+    IERC20(token1).transfer(
+        owner,
+        IERC20(token1).balanceOf(address(this))
+    );
 
+}     
+
+    //////////////////////////////////////////////////////////////////////////////
+    // NOTES
+    // FLOW: 
+    // Flashloan in USD 
+    // Buy Tokens on AMM1: swap Tokens USD for SOB on AMM1 >> buy SOB tokens 
+    // Swap/sell Tokens on AMM2: SOB for USD on AMM2
+    // calculate Withdraw amount then removeLiquidity and payback Loan
+    
+
+    function arbitrage(
+            AMM1,
+            AMM2,
+            address flashToken,
+            address arbToken,
+            uint256 flashAmount
+            
+            
+        ) external ReentrancyGuard {
+            
+        // track balances of tokens
+        uint256 arbAmount = flashAmount;
+        uint256 arbBalance = arbToken.balanceOf(address(this));
+        
+        address[] memory path = new address[](2);
+            path[0] = arbToken;
+            path[1] = flashToken;
+        
+                
+        
+        // take flashtoken and swap on AMM1 for arbtoken
+        // Swap USD for SOB tokens
+        arbToken.approve(address(AMM1), arbAmount);     
+        AMM1.swapToken2(arbAmount);
+        
+        require(arbAmount > 0, "Must buy at least one token");
+
+
+        // check balance of arbtoken
+        uint256 balanceBefore = AMM2(token1).balanceOf(address(this));
+        uint256 balanceAfter = arbToken.balanceOf(address(this)) + flashAmount;
+        require(balanceAfter >= arbAmount, "Not enough tokens to Trade");
+
+
+        // swap all arbtokens on Amm2 for flashtoken 
+        // check balance of flashtoken
+        // Swap SOB tokens for USD
+        arbToken.approve(address(AMM2), arbAmount);
+        AMM2.swapToken1(arbAmount);
+        uint256 flashBalance = flashToken.balanceOf(address(this)); 
+
+
+        // subtract of flashtoken from flashloan amount to determine profit        
+        // Calculate Withdraw Amount and then Remove Liquidity on AMM2
+        uint256 profit = flashBalance - flashAmount;
+        arbToken.approve(address(AMM2), arbAmount);
+        uint256 withDrawAmount = AMM2.calculateWithdrawAmount(profit);
+        AMM2.removeLiquidity(withDrawAmount);
+
+        balanceAfter = arbToken.balanceOf(address(this));
+        require(balanceAfter >= balanceBefore, "Arbitrage was not successful");
+
+        // payback flashloan        
+        // send profit to dev/user       
+    }    
 }
