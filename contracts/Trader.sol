ee4fba9a62dca2387f4035552ed2d427e909512b
John <jastek@SEBEK.localdomain>
2023-03-18 23:03:53 -0700

    WIP:arbitrage function

--- contracts/Trader.sol
+++ contracts/Trader.sol
@@ -11,20 +11,18 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 
-contract Trader {
+contract Trader is ReentrancyGuard {
     // pass in FLP contract address save to State var - NOTE: must deploy FLP first. Be sure to pass tokens in test
    
     address flashLoanPoolAddress; // NEED FlashloanPool address   
-    address public owner;
-    address token1;
-    address constant AMM1_ADDRESS = 0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9;
-    address constant AMM2_ADDRESS = 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0;
-    address constant amm1Token1 = 0x5FbDB2315678afecb367f032d93F642f64180aa3; // SOB
-    address constant amm2Token2 = 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512; // USD
+    address public owner; 
+    address AMM1_ADDRESS;     // should this be in the constructor
+    address AMM2_ADDRESS;     // should this be in the contsructor
+    address token1;  // SOB
+    address token2;  // USD
     
 
-    // EVENTS
-   
+    // EVENTS   
     event Loan(            
         address tokenGive,    
         uint256 tokenGiveAmount      
@@ -42,22 +40,25 @@ contract Trader {
         // );
 
 
-    constructor(address _token1, address _flashLoanPoolAddress) payable {
-        owner = msg.sender;
+    constructor(address _token1, address _token2, address _flashLoanPoolAddress, address _AMM1_ADDRESS, address _AMM2_ADDRESS) payable {
+        owner = msg.sender;        
         token1 = _token1;   // USD token in Flash pool
-        flashLoanPoolAddress = _flashLoanPoolAddress;       
-    }     
+        token2 = _token2;
+        flashLoanPoolAddress = _flashLoanPoolAddress;
+        AMM1_ADDRESS = _AMM1_ADDRESS;
+        AMM2_ADDRESS = _AMM2_ADDRESS;
+    }   
     
-    function flashLoan(uint256 _borrowAmount) public {
+    function flashLoan(uint256 _borrowAmount) public { // getting USD ammtoken2
         FlashLoanPool(flashLoanPoolAddress).flashLoan(_borrowAmount);
     }  
 
     // same as FLP receive function
-    function receiveTokens(address _token1, uint256 _borrowAmount) public payable {
-    console.log('Loan Received (in wei)', _token1, _borrowAmount);
+    function receiveTokens(address _flashToken, uint256 _borrowAmount) public payable {
+    console.log('Loan Received (in wei)', token1, _borrowAmount);
 
      emit Loan(             // Emit event to prove tokens receive in test     
-        address(token1),    // tokenGive,
+        address(_flashToken),
         _borrowAmount      // tokenGiveAmount       
         );
                 
@@ -66,7 +67,9 @@ contract Trader {
     // Arbitrage here buy on AMM1 and sell on AMM2
     // TODO: advanced - use script to get prices to change which will call trader contract to buy and sell     
     
-      arbitrage(AMM1_ADDRESS, AMM2_ADDRESS, _token1, _borrowAmount );
+      // How and what vars to add when calling function here?
+      // 
+      arbitrage(token1, token2, _borrowAmount );
 
     
     // Return all tokens to the Pool
@@ -76,7 +79,6 @@ contract Trader {
         owner,
         IERC20(token1).balanceOf(address(this))
     );
-
 }
 
     //////////////////////////////////////////////////////////////////////////////
@@ -89,24 +91,22 @@ contract Trader {
     
 
     function arbitrage(            
-        address flashToken,
-        address arbToken,
-        uint256 flashAmount            
-    ) external ReentrancyGuard {
+        address _flashToken,
+        address _arbToken,       
+        uint256 _flashAmount            
+    ) public  {        // ReentrancyGuard
             
         // track balances of tokens
-        uint256 arbAmount = flashAmount;
-        uint256 arbBalance = IERC20(arbToken).balanceOf(address(this));
-        
-        // address[] memory path = new address[](2);
-        //     path[0] = arbToken;
-        //     path[1] = flashToken;                
+        uint256 arbAmount = _flashAmount;
+        uint256 arbBalance = IERC20(_arbToken).balanceOf(address(this));
         
         // take flashtoken and swap on AMM1 for arbtoken
         // Swap USD for SOB tokens
-        IERC20(arbToken).approve(address(AMM), arbAmount);     
+        IERC20(_arbToken).approve(address(AMM1_ADDRESS), arbAmount);        
         AMM(AMM1_ADDRESS).swapToken2(arbAmount);
         
+    // Test on each step can remove test after sauccess
+
         // require(arbAmount > 0, "Must buy at least one token");
 
 
