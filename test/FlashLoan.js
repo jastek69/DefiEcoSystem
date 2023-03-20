150214aeb83992b5504d69e6adfd59919be2df0a
John <jastek@SEBEK.localdomain>
2023-03-20 13:35:05 -0700

    WIP: arbitrage test - liquidity tests

--- test/FlashLoan.js
+++ test/FlashLoan.js
@@ -17,8 +17,7 @@ describe('FlashLoan', () => {
       investor1,
       investor2
 
-  let token,
-      token1,
+  let token1,
       token2,
       amm1,
       amm2
@@ -36,14 +35,14 @@ describe('FlashLoan', () => {
     const Token = await ethers.getContractFactory('Token')
       token1 = await Token.deploy('USD Token', 'USD', tokens('1000000000')) // Deploy 1 Billion tokens
      // token1 = await Token.deploy('USD Token', 'USD', tokens('1000000'))
-      token2 = await Token.deploy('Sobek Token', 'SOB', tokens('500000'))
+      token2 = await Token.deploy('Sobek Token', 'SOB', tokens('1000000000'))
 
     
     // Send tokens to liquidity provider
-    let transaction = await token1.connect(deployer).transfer(liquidityProvider.address, tokens(1000000)) // NOTE: use 'connect' to connect to a contract
+    let transaction = await token1.connect(deployer).transfer(liquidityProvider.address, tokens(10000000)) // NOTE: use 'connect' to connect to a contract
     await transaction.wait()
 
-    transaction = await token2.connect(deployer).transfer(liquidityProvider.address, tokens(1000000)) // NOTE: "let" already done in first declaration
+    transaction = await token2.connect(deployer).transfer(liquidityProvider.address, tokens(10000000)) // NOTE: "let" already done in first declaration
     await transaction.wait()
 
 
@@ -99,17 +98,28 @@ describe('FlashLoan', () => {
     let amm1SobAmount = tokens(1000000)
     
     //Liquidity Provider Approves tokens on AMM1
-    transaction = await token1.connect(liquidityProvider).approve(amm1.address, amm1UsdAmount) // first person to add liquidity so sets the price
+    transaction = await token1.connect(liquidityProvider).approve(amm1.address, amm1UsdAmount)
     await transaction.wait()
 
     transaction = await token2.connect(liquidityProvider).approve(amm1.address, amm1SobAmount)
     await transaction.wait()
-
-
     
     // Liquidity Provider Adds liquidity on AMM1
     transaction = await amm1.connect(liquidityProvider).addLiquidity(amm1UsdAmount, amm1SobAmount)
     await transaction.wait()
+
+
+    // Get current AMM1 Pool Balance 
+    console.log(`AMM1 USD Token Balance: ${ethers.utils.formatEther(await amm1.token1Balance())} \n`)
+    console.log(`AMM1 Sobek Token Balance: ${ethers.utils.formatEther(await amm1.token2Balance())} \n`)
+
+    // Check AMM1 receives tokens
+    expect(await token1.balanceOf(amm1.address)).to.equal(amm1UsdAmount)
+    expect(await token2.balanceOf(amm1.address)).to.equal(amm1SobAmount)
+
+    expect(await amm1.token1Balance()).to.equal(amm1UsdAmount)
+    expect(await amm1.token2Balance()).to.equal(amm1SobAmount)
+
 
 
     // Add Liquidity to AMM2
@@ -123,11 +133,22 @@ describe('FlashLoan', () => {
     transaction = await token2.connect(liquidityProvider).approve(amm2.address, amm2SobAmount)
     await transaction.wait()
 
-
     // Liquidity Provider Adds liquidity to AMM2
     transaction = await amm2.connect(liquidityProvider).addLiquidity(amm2UsdAmount, amm2SobAmount)
     await transaction.wait()
 
+
+    // Get current AMM2 Pool Balance 
+    console.log(`AMM2 USD Token Balance: ${ethers.utils.formatEther(await amm2.token1Balance())} \n`)
+    console.log(`AMM2 Sobek Token Balance: ${ethers.utils.formatEther(await amm2.token2Balance())} \n`)
+
+
+    // Check AMM2 receives tokens
+    expect(await token1.balanceOf(amm2.address)).to.equal(amm2UsdAmount)
+    expect(await token2.balanceOf(amm2.address)).to.equal(amm2SobAmount)
+
+    expect(await amm2.token1Balance()).to.equal(amm2UsdAmount)
+    expect(await amm2.token2Balance()).to.equal(amm2SobAmount)
   
   })
  
