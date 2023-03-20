44c09337fc00782d21729e68a51fce9ac7fdf89b
John <jastek@SEBEK.localdomain>
2023-03-20 12:52:30 -0700

    WIP:arbitrage test update - add liquidity

--- test/FlashLoan.js
+++ test/FlashLoan.js
@@ -34,16 +34,16 @@ describe('FlashLoan', () => {
 
     // Deploy Token
     const Token = await ethers.getContractFactory('Token')
-      token = await Token.deploy('USD Token', 'USD', tokens('1000000000')) // Deploy 1 Billion tokens
-      token1 = await Token.deploy('USD Token', 'USD', tokens('1000000'))
+      token1 = await Token.deploy('USD Token', 'USD', tokens('1000000000')) // Deploy 1 Billion tokens
+     // token1 = await Token.deploy('USD Token', 'USD', tokens('1000000'))
       token2 = await Token.deploy('Sobek Token', 'SOB', tokens('500000'))
 
     
     // Send tokens to liquidity provider
-    let transaction = await token1.connect(deployer).transfer(liquidityProvider.address, tokens(100000)) // NOTE: use 'connect' to connect to a contract
+    let transaction = await token1.connect(deployer).transfer(liquidityProvider.address, tokens(1000000)) // NOTE: use 'connect' to connect to a contract
     await transaction.wait()
 
-    transaction = await token2.connect(deployer).transfer(liquidityProvider.address, tokens(100000)) // NOTE: "let" already done in first declaration
+    transaction = await token2.connect(deployer).transfer(liquidityProvider.address, tokens(1000000)) // NOTE: "let" already done in first declaration
     await transaction.wait()
 
 
@@ -94,6 +94,40 @@ describe('FlashLoan', () => {
       })
     })
 
+    // Add Liquidity to AMM1
+    let amm1UsdAmount = tokens(1000000)
+    let amm1SobAmount = tokens(1000000)
+    
+    //Liquidity Provider Approves tokens on AMM1
+    transaction = await token1.connect(liquidityProvider).approve(amm1.address, amm1UsdAmount) // first person to add liquidity so sets the price
+    await transaction.wait()
+
+    transaction = await token2.connect(liquidityProvider).approve(amm1.address, amm1SobAmount)
+    await transaction.wait()
+
+
+    
+    // Liquidity Provider Adds liquidity on AMM1
+    transaction = await amm1.connect(liquidityProvider).addLiquidity(amm1UsdAmount, amm1SobAmount)
+    await transaction.wait()
+
+
+    // Add Liquidity to AMM2
+    let amm2UsdAmount = tokens(1000000)
+    let amm2SobAmount = tokens(500000)
+    
+    //Liquidity Provider Approves tokens on AMM2
+    transaction = await token1.connect(liquidityProvider).approve(amm2.address, amm2UsdAmount) // first person to add liquidity so sets the price
+    await transaction.wait()
+
+    transaction = await token2.connect(liquidityProvider).approve(amm2.address, amm2SobAmount)
+    await transaction.wait()
+
+
+    // Liquidity Provider Adds liquidity to AMM2
+    transaction = await amm2.connect(liquidityProvider).addLiquidity(amm2UsdAmount, amm2SobAmount)
+    await transaction.wait()
+
   
   })
  
@@ -114,7 +148,7 @@ describe('FlashLoan', () => {
     // Deploy Flash Loan Pool contract
     console.log(`Deploying FlashLoanPool contract...\n`);
     const FlashLoanPool = await ethers.getContractFactory("FlashLoanPool")
-    const flashLoanPool = await FlashLoanPool.deploy(token.address)
+    const flashLoanPool = await FlashLoanPool.deploy(token1.address)
     await flashLoanPool.deployed()
     console.log(`FlashLoanPool deployed to: ${flashLoanPool.address}\n`);
 
@@ -124,14 +158,14 @@ describe('FlashLoan', () => {
     console.log (`Transferring tokens to FlashLoanPool\n`);
     let amount = tokens(1000000000)
     let borrowAmount = tokens(1000000)
-    let transaction = await token.connect(deployer).approve(flashLoanPool.address, amount)
+    let transaction = await token1.connect(deployer).approve(flashLoanPool.address, amount)
     await transaction.wait()
    
     await flashLoanPool.connect(deployer).depositTokens(amount)
     await transaction.wait()
 
     // LoanPool Balance
-    let poolBalance = await token.balanceOf(flashLoanPool.address)
+    let poolBalance = await token1.balanceOf(flashLoanPool.address)
     expect(poolBalance).to.equal(amount)
     console.log(`Transferred Tokens to pool (in wei): ${amount}\n`); 
 
@@ -139,14 +173,14 @@ describe('FlashLoan', () => {
     // Deploy Trader contract
     console.log(`Deploying Trader contract...\n`)
     const Trader = await ethers.getContractFactory("Trader")
-    const trader = await Trader.deploy(token.address, token1.address, flashLoanPool.address, amm1.address, amm2.address)
+    const trader = await Trader.deploy(token1.address, token2.address, flashLoanPool.address, amm1.address, amm2.address)
     await trader.deployed()
     console.log(`Trader deployed to: ${trader.address}\n`);
     
 
     // Call FlashLoan    
     console.log(`Calling Flashloan`);
-    await token.connect(borrower).approve(trader.address, borrowAmount);  
+    await token1.connect(borrower).approve(trader.address, borrowAmount);  
     transaction = await trader.connect(borrower).flashLoan(borrowAmount);
     await transaction.wait()
      
@@ -159,7 +193,7 @@ describe('FlashLoan', () => {
 
     // Use Emit Event to confirm loan payment
     await expect(transaction).to.emit(trader, 'Loan').withArgs( 
-      token.address,   
+      token1.address,   
       borrowAmount
       )     
 
@@ -183,7 +217,7 @@ describe('FlashLoan', () => {
 
     // Check Payback Loan
     console.log(`Paying Back FlashLoan`);
-    poolBalance = await token.balanceOf(flashLoanPool.address)
+    poolBalance = await token1.balanceOf(flashLoanPool.address)
     expect(poolBalance).to.equal(amount)
     console.log(`Transferred Tokens to pool: ${amount}\n`);
 
